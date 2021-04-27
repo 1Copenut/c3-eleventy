812f3b57b869a55e33cbadfd50ec3f2df6fadd2f
1Copenut <pierce.trevor@gmail.com>
2021-04-27 07:47:32 -0500

    WIP. Trying to add PostCSS.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (eleventyConfig) {
   return {
     dir: {
-      input: 'src/site',
+      input: 'src',
       output: 'dist',
     },
 
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
-    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    templateFormats: ['md', 'njk', 'html', '11ty.js'],
 
     // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: 'njk',
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Continuum Design v3.dough
 
-## Eleventy References I Use Alot
+## Eleventy References I Use A Lot
 
 - https://www.11ty.dev/docs/usage/
+- https://www.11ty.dev/docs/config/#transforms
+
+## Git References I Use Sometimes
+
+- https://alvinalexander.com/git/git-rm-file-git-repository-dont-track-accidentally-added/
