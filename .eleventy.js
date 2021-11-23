fc987a13fa3a3226c112bfc75be0221a189cfa26
Trevor Pierce <pierce.trevor@gmail.com>
2021-11-22 23:15:00 -0600

    Fixed pagination, sans styling.

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy(".well-known/humans.txt");
 
-  // eleventyConfig.addCollection('services', function (collection) {
-  //   const coll = collection.getFilteredByTag('services');
-
-  //   for (let i = 0; i < coll.length; i++) {
-  //     const prevPost = coll[i - 1];
-  //     const nextPost = coll[i + 1];
-
-  //     coll[i].data['prevPost'] = prevPost;
-  //     coll[i].data['nextPost'] = nextPost;
-  //   }
-
-  //   return coll;
-  // });
-
   return {
     dir: {
       input: "src",
