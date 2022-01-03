0f523fd6697e91b00aa5534ab585edadfffa1220
1Copenut <pierce.trevor@gmail.com>
2022-01-03 15:47:05 -0600

    Finalizing CSS minifcation using Promises.

--- src/processors/postcss.11ty.js
+++ src/processors/postcss.11ty.js
@@ -1,22 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 const postcss = require('postcss');
+const postcssImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
+const cleanCSS = require("clean-css");
 
-const fileName = 'main.css';
 
 module.exports = class {
   async data() {
+    const fileName = 'main.css';
+    const destFilePath = path.join(__dirname, `../../dist/css/${fileName}`);
     const rawFilepath = path.join(__dirname, `../_includes/css/${fileName}`);
+
     return {
-      permalink: `css/${fileName}`,
       rawFilepath,
+      destFilePath,
       rawCss: await fs.readFileSync(rawFilepath),
     };
   }
 
-  async render({ rawCss, rawFilepath }) {
-    return await postcss([require('postcss-import'), require('postcss-nested')])
+  async render({ rawCss, rawFilepath, destFilePath }) {
+    return await postcss([postcssImport, postcssNested])
       .process(rawCss, { from: rawFilepath })
-      .then((result) => result.css);
+      .then((result) => result.css)
+      .then((result) => {
+        new cleanCSS({ returnPromise : true })
+          .minify(result)
+          .then((result) => {
+            console.log({result});
+            fs.writeFileSync(destFilePath, result.styles, { encoding: "utf-8" });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 };
