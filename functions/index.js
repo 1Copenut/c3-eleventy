454a13f078dcafd2a2a8dae273f1135783a8f71b
1Copenut <pierce.trevor@gmail.com>
2022-12-11 17:03:09 -0600

    Adding basic CSP headers.

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,17 @@ export const onRequest = async ({ request, next, env }) => {
   const response = await next();
 
   if (contentType.includes("text/html")) {
+    // Add basic CSP headers first
+    response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+    response.headers.set("X-Content-Type-Options", "nosniff");
+    response.headers.set("X-Frame-Options", "SAMEORIGIN");
+    response.headers.set("X-XSS-Protection", "1; mode=block");
+    response.headers.set(
+      "Content-Security-Policy",
+      `default-src 'self'; base-uri 'none'; object-src 'none'; connect-src 'none'; frame-src https://challenges.cloudflare.com; img-src 'self' data; style-src 'self'; script-src 'self' 'sha256-0dhzRub3e7mU2g5sd8vHUEe971OY216EW+BnNkWNaQs=' https://challenges.cloudflare.com; frame-ancestors 'none'; require-trusted-types-for 'script';`
+    );
+
+    // Find the nonce string and replace it
     return new HTMLRewriter()
       .on(
         "script",
@@ -15,9 +26,6 @@ export const onRequest = async ({ request, next, env }) => {
   } else {
     return response;
   }
-  // response.headers.set("Access-Control-Allow-Origin", "*");
-  // response.headers.set("Access-Control-Max-Age", "86400");
-  // return response;
 };
 
 class AttributeWriter {
