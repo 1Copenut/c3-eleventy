4a00d06dac331b032c9b17e91fe320e8e5fada24
1Copenut <pierce.trevor@gmail.com>
2022-12-21 19:41:19 -0600

    Refactored CSP to insert only on 200 OK status.

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,21 @@ import { v4 } from "uuid";
 export const onRequest = async ({ request, next, env }) => {
   const NONCE_SECRET = env.NONCE_SECRET;
   const NONCE_TOKEN = nonceGenerator();
-  const response = await next();
 
-  if (response.status === 304) {
-    return response;
-  } else {
+  const response = await next();
+  const headers = Object.fromEntries(response.headers);
+  const contentType = headers["content-type"];
+
+  // 200 OK. Set CSP headers
+  if (contentType && contentType.startsWith("text/html")) {
+    response.headers.set("cf-nonce-generator", "HIT");
     response.headers.set(
       "Content-Security-Policy",
       `default-src 'self'; base-uri 'none'; object-src 'none'; connect-src 'none'; frame-src https://challenges.cloudflare.com; img-src 'self' data; style-src 'self'; script-src 'self' 'nonce-${NONCE_TOKEN}' https://challenges.cloudflare.com; frame-ancestors 'none'; require-trusted-types-for 'script';`
     );
-    response.headers.set("cf-nonce-generator", "HIT");
+    response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+    response.headers.set("X-Frame-Options", "SAMEORIGIN");
+    response.headers.set("X-XSS-protection", "1; mode=block");
 
     // Find the nonce string and replace it
     const rewriter = new HTMLRewriter()
@@ -22,6 +27,13 @@ export const onRequest = async ({ request, next, env }) => {
 
     return rewriter;
   }
+
+  // 304 not modified. No need to add CSP headers.
+  if (response.status === 304) {
+    return response;
+  }
+
+  return response;
 };
 
 class AttributeWriter {
