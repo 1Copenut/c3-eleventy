44d8ea581ebc637fec423ab4735abd81daf6b737
1Copenut <pierce.trevor@gmail.com>
2023-07-23 19:06:56 -0500

    Fixed telephone regex bug. Moved prod URLs to data.config.

--- src/_data/config.js
+++ src/_data/config.js
@@ -6,5 +6,9 @@ module.exports = {
   title: "Continuum Design. Work for the digital age.",
   nonceSecret: process.env.NONCE_SECRET,
   environment: process.env.ENVIRONMENT,
+  sentryScriptURL:
+    "https://js.sentry-cdn.com/3e24862ff9ce4761ab71fce722fc4c6b.min.js",
   turnstileScriptURL: "https://challenges.cloudflare.com/turnstile/v0/api.js",
+  umamiDataWebsiteId: "55ab36ac-ae04-4894-9de9-624f30ce6339",
+  umamiScriptURL: "https://analytics.umami.is/script.js",
 };
