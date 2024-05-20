3b3ec300e280c7a0a955f1332941ffc493e08954
1Copenut <pierce.trevor@gmail.com>
2024-05-19 19:42:47 -0500

    Adding two Umami URLs for analytics CSP.

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -15,7 +15,8 @@ export const onRequest = async ({ request, next, env }) => {
     response.headers.set("cf-nonce-generator", "HIT");
     response.headers.set(
       "Content-Security-Policy",
-      `default-src 'self'; base-uri 'none'; object-src 'none'; connect-src https://sentry.io/ https://analytics.umami.is/ https://contactform.continuumdesign.net/; frame-src https://challenges.cloudflare.com; img-src 'self' data; style-src 'self'; script-src 'strict-dynamic' 'nonce-${NONCE_TOKEN}'; frame-ancestors 'none'; require-trusted-types-for 'script'; report-uri https://o1405800.ingest.sentry.io/api/6739194/security/?sentry_key=3e24862ff9ce4761ab71fce722fc4c6b;`
+      `default-src 'self'; base-uri 'none'; object-src 'none'; connect-src https://sentry.io/ https://analytics.umami.is/ https://api-gateway.umami.dev
+ https://gateway.umami.is https://contactform.continuumdesign.net/; frame-src https://challenges.cloudflare.com; img-src 'self' data; style-src 'self'; script-src 'strict-dynamic' 'nonce-${NONCE_TOKEN}'; frame-ancestors 'none'; require-trusted-types-for 'script'; report-uri https://o1405800.ingest.sentry.io/api/6739194/security/?sentry_key=3e24862ff9ce4761ab71fce722fc4c6b;`
     );
     response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
     response.headers.set("X-Frame-Options", "SAMEORIGIN");
